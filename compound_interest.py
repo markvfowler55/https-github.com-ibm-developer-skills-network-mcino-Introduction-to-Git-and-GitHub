972f33f190daa5af1ea6fe35c8a83b2dc3d4a342
nikhil kumar <nikhilkumar02102005@gmail.com>
2025-10-17 15:57:40 +0530

    initial changes

--- compound_interest.py
+++ compound_interest.py
@@ -16,9 +16,10 @@ def compound_interest(p, t, r):
     return p * (pow((1 + r / 100), t))
 
 
-if __name__ == "__main__":
+if __name__ == "___main___":
     p = float(input("Enter the principal amount: "))
     t = float(input("Enter the time period: "))
     r = float(input("Enter the rate of interest: "))
 
     print("The compound interest is {:.2f}".format(compound_interest(p, t, r)))
+    print("Changed")
