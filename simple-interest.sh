8d54f579acdfa93647b63d9eda37fadcca724fa7
LUAI ALARIFY <loayalarify777184509@gmail.com>
2026-03-08 03:49:56 +0300

    Fix comments and calculation for simple interest

--- simple-interest.sh
+++ simple-interest.sh
@@ -3,8 +3,8 @@
 # Do not use this in production. Sample purpose only.
 
 # Author: Upkar Lidder (IBM)
-# Addtional Authors:
-# <your Github username>
+# Additional Authors:
+# loaycpp
 
 # Input:
 # p, principal amount
@@ -12,15 +12,18 @@
 # r, annual rate of interest
 
 # Output:
-# simple interest = p*t*r
+# simple interest = p*t*r/100
 
 echo "Enter the principal:"
 read p
+
 echo "Enter rate of interest per year:"
 read r
+
 echo "Enter time period in years:"
 read t
 
 s=$(expr $p \* $t \* $r / 100)
-echo "The simple interest is: "
+
+echo "The simple interest is:"
 echo $s
