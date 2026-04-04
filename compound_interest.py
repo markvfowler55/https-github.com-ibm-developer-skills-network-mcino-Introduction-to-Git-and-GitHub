063e15d959b44ed962e35649a3e17806014956bc
Ishan1012 <ishandwivedi1210@gmail.com>
2026-04-05 01:46:49 +0530

    minor bugs fixed and increased decimal to 3 places

--- compound_interest.py
+++ compound_interest.py
@@ -11,14 +11,12 @@
 # Output:
 # compound interest = p * (1 + r/100)^t
 
-
 def compound_interest(p, t, r):
     return p * (pow((1 + r / 100), t))
 
-
 if __name__ == "__main__":
-    p = float(input("Enter the principal amount: "))
-    t = float(input("Enter the time period: "))
-    r = float(input("Enter the rate of interest: "))
+    p = float(input("Enter the principal amount:"))
+    t = float(input("Enter the time period:"))
+    r = float(input("Enter the rate of interest:"))
 
-    print("The compound interest is {:.2f}".format(compound_interest(p, t, r)))
+    print("The compound interest is {:.3f}".format(compound_interest(p, t, r)))
